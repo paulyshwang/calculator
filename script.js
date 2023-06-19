5ccc1783317cea66d4a620385a9468c25acc0b10
Paul YS Hwang <109876624+paulyshwang@users.noreply.github.com>
2023-06-19 02:05:10 -0400

    Create operate() which calls previous functions
    Also, add variables for an operator and two numbers to use later

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let operator = "";
+let firstNum = 0;
+let secondNum = 0;
+
 function add(a, b) {
   return a + b;
 };
@@ -12,4 +16,11 @@ function multiply(a, b) {
 
 function divide(a, b) {
   return a / b;
+};
+
+function operate(operator, firstNum, secondNum) {
+  if (operator === "+") return add(firstNum, secondNum);
+  if (operator === "-") return subtract(firstNum, secondNum);
+  if (operator === "*") return multiply(firstNum, secondNum);
+  if (operator === "/") return divide(firstNum, secondNum);
 };
